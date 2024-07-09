3cda78496b12aad847ddd533475591d3a174c9d1
Chathuracp <cpchathura7@gmail.com>
2024-07-09 08:57:36 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,"PRABATH-MD~g68CXbLI#E2EkJgI1qOb1lYNubEt9qjqmHmFf5WBJ3dj-y-6TCPs"
-BOT_NUMBER: process.env.BOT_NUMBER,94765600495,
-GITHUB_USERNAME: process.env.GITHUB_USERNAME,",Chathuracp"
+SESSION_ID: process.env.SESSION_ID,
+BOT_NUMBER: process.env.BOT_NUMBER,
+GITHUB_USERNAME: process.env.GITHUB_USERNAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
 };
